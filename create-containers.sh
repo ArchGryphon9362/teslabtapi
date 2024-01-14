92db4e1c992a62298f2c216632e946e6337f6804
Lex Nastin <lexnastin@gmail.com>
2024-01-14 21:44:28 +0000

    fix docker build and push

--- create-containers.sh
+++ create-containers.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 git pull
-docker build -t ghcr.io/archgryphon9362/teslabtapi .
-docker push ghcr.io/archgryphon9362/teslabtapi
+docker build -t ghcr.io/archgryphon9362/teslabtapi2 .
+docker push ghcr.io/archgryphon9362/teslabtapi2
